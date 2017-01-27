2d12fd29d9ece1f3d94a609fe03cfcc22540b231
clevermm <mm138111@gmail.com>
2017-01-27 10:03:14 +0330

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,135 @@
+<?php
+define('API_KEY','275661249:AAF6nBpKZK9xlXLPezgjFy8uLeABix0IzBY');
+//----######------
+function makereq($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+$result=json_decode($message,true);
+//##############=--API_REQ
+function apiRequest($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+  foreach ($parameters as $key => &$val) {
+    // encoding to JSON array parameters, for example reply_markup
+    if (!is_numeric($val) && !is_string($val)) {
+      $val = json_encode($val);
+    }
+  }
+  $url = "https://api.telegram.org/bot".API_KEY."/".$method.'?'.http_build_query($parameters);
+  $handle = curl_init($url);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+  return exec_curl_request($handle);
+}
+//----######------
+//---------
+$update = json_decode(file_get_contents('php://input'));
+var_dump($update);
+//=========
+$chat_id = $update->message->chat->id;
+$message_id = $update->callback_query->message->message_id;
+$from_id = $update->message->from->id;
+$name = $update->message->from->first_name;
+$username = $update->message->from->username;
+$t = isset($update->message->text)?$update->message->text:'';
+$reply = $update->message->reply_to_message->forward_from->id;
+$sticker = $update->message->sticker;
+$text = $update->message->text;
+$result = json_decode($message,true);
+$data = $update->callback_query->data;
+$forward = $update->message->forward_from;
+$reply = $update->message->reply_to_message->forward_from->id;
+$time = file_get_contents("http://api.provps.ir/td/?td=time");
+$date = file_get_contents("http://api.provps.ir/td/?td=date");
+//-------
+function SendMessage($ChatId, $TextMsg)
+{
+ makereq('sendMessage',[
+'chat_id'=>$ChatId,
+'text'=>$TextMsg,
+'parse_mode'=>"MarkDown"
+]);
+}
+function SendM($ChatId, $TextMsg)
+{
+ makereq('sendMessage',[
+'chat_id'=>$ChatId,
+'text'=>$TextMsg,
+'parse_mode'=>"html"
+]);
+}
+function SendSticker($ChatId, $sticker_ID)
+{
+ makereq('sendSticker',[
+'chat_id'=>$ChatId,
+'sticker'=>$sticker_ID
+]);
+}
+function bots($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+function typing($chat_id)
+{
+file_get_contents(API_TELEGRAM.'sendChatAction?chat_id='.$chat_id.'&action=typing');
+}
+function Forward($KojaShe,$AzKoja,$KodomMSG)
+{
+makereq('ForwardMessage',[
+'chat_id'=>$KojaShe,
+'from_chat_id'=>$AzKoja,
+'message_id'=>$KodomMSG
+]);
+}
+function save($filename,$TXTdata)
+  {
+  $myfile = fopen($filename, "w") or die("Unable to open file!");
+  fwrite($myfile, "$TXTdata");
+  fclose($myfile);
+  }
+//==========
+if(preg_match("/^\/(Clever)/",$t)){
+$ax="http://img15.deviantart.net/c893/i/2007/354/d/a/clever_fox_logo_by_trixyrogue.jpg";
+bots('sendphoto',[
+'chat_id'=>$chat_id,
+'photo'=>$ax,
+'caption'=>"Clever The Fox",
+]);
+}
+if(preg_match("/^\/(start)/",$t)){
+$seda="http://sv.darvishmusic.com/music/95/dey/2/Hichkas%20-%20Gerye%20Dare%20[128].mp3";
+bots('sendaudio',[
+'chat_id'=>$chat_id,
+'audio'=>$seda,
+'caption'=>"test :| gerye dare",
+]);
+}
+?>
